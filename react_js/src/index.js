a75ddb95c37e69c15e7fb62d50aaaf93988e9c8f
chench <changhua.chen@qcast.cn>
2020-01-06 19:09:11 +0800

    react_js:tabWidgetSample:添加sample

--- react_js/src/index.js
+++ react_js/src/index.js
@@ -5,8 +5,8 @@ import './index.css';
 //import App from './simpleMetroWidget/App'; // 简易版本的SimpleWidget样例
 //import App from './advanceMetroWidget/App'; // 升级版本的SimpleWidget例子(MetroWidget嵌套)
 //import App from './videoDemo/App'; // 视频播放控制demo
-// import App from './showcaseDemo/App'; // 橱窗界面Demo(SimpleWidget的一个使用场景)
-import App from './tabWidgetSample/App'
+import App from './showcaseDemo/App'; // 橱窗界面Demo(SimpleWidget的一个使用场景)
+// import App from './tabWidgetSample/App'
 
 if(!!window.JsView) { // 如果使用JsView
 	// (可选配置)按键接受的扩展，例如将静音按键(JAVA键值为164)映射为JS键值20001，PS:注意"164"的引号
