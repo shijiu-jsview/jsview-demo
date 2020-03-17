ffca3855613b14d167aa7471a0ec478cda57fcf5
ludl <donglin.lu@qcast.cn>
2020-03-17 11:44:01 +0800

    java_app:build.gradle:调整core.aar的引用方式,不再需要每次调版本信息;react_js:jsview-react的引用方式从update.sh移动到package.json中

--- react_js/patch_package/update.sh
+++ react_js/patch_package/update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo UPDATE jsview-react
-cp -a jsview-react ../node_modules/
+
+# 已经在package.json做关联,不需在此拷贝了
+#echo UPDATE jsview-react
+#cp -a jsview-react ../node_modules/
 
 echo UDPATE react-script/config/webpack.config.js
 cp react-scripts/config/webpack.config.js ../node_modules/react-scripts/config/webpack.config.js
