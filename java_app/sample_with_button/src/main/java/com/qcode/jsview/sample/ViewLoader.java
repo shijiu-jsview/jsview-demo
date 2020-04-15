87177d560a1befceba0a985d0b6a71cda9c1b95a
ludl <donglin.lu@qcast.cn>
2020-04-15 14:44:05 +0800

    java_app:调整sample_with_button的APK的编译方式,loadUrl2接口传入的两个参数可在gradlew编译时配置,用于TestCase项目中编译测试用APK

--- java_app/sample_with_button/src/main/java/com/qcode/jsview/sample/ViewLoader.java
+++ java_app/sample_with_button/src/main/java/com/qcode/jsview/sample/ViewLoader.java
@@ -16,6 +16,9 @@ public class ViewLoader {
 	private JsView mJsView;
 	private long mLastKeyUpTime = 0;
 
+	private static final String sJsViewEngineUrl = BuildConfig.JSVIEW_JS_ENGINE_URL;
+	private static final String sAppUrl = BuildConfig.APP_URL;
+
 	public ViewLoader(Activity activity) {
 		mActivity = activity;
 
@@ -69,9 +72,9 @@ public class ViewLoader {
 
 		// JsView加载URL
 		mJsView.loadUrl2(
-				"http://cdn.release.qcast.cn/forge_js/master/JsViewES6_react_r670.jsv.3a1d74c1.js",
+				sJsViewEngineUrl,
 				// TODO: 此处改为react运行的主JS对应的地址，一版为 http://PC-IP:3000 下 /static/js/bundle.js
-				"http://192.168.0.32:3000/static/js/bundle.js");
+				sAppUrl);
 
 		mJsView.requestFocus();
 	}
