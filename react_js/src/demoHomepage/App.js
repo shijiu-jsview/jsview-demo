c37acc9f507b056b5dd1ea21f953fe21f4469482
chench <changhua.chen@qcast.cn>
2020-04-16 10:50:14 +0800

    react-js: demoHomepage:删除误传的长文字demo

--- react_js/src/demoHomepage/App.js
+++ react_js/src/demoHomepage/App.js
@@ -86,11 +86,11 @@ let demoInfos = [
         "path": "/users/basicFdivControl",
         "class": lazy(() => import('../basicFdivControl/App')),
     },
-    {
-        "name": "长文字",
-        "path": "/users/longText",
-        "class": lazy(() => import('../longText/App')),
-    },
+    // {
+    //     "name": "长文字",
+    //     "path": "/users/longText",
+    //     "class": lazy(() => import('../longText/App')),
+    // },
 ]
 
 let color = ["#823935", "#89BEB2", "#C9BA83", "#DED38C", "#DE9C53"];
@@ -117,11 +117,7 @@ class HomePageProxy extends React.Component{
 class App extends React.Component {
     constructor(props) {
         super(props);
-
-
         this._FocusControl = null;
-
-
         this.state = {
             "homepageDisplay": "visible"
         };
