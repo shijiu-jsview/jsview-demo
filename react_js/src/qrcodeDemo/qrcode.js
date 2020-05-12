89968df43250fdd7c6698006546027a09fa7ed7e
ludl <donglin.lu@qcast.cn>
2020-05-12 13:32:25 +0800

    react_js:QRCode:完成Demo的说明文档

--- react_js/src/qrcodeDemo/qrcode.js
+++ react_js/src/qrcodeDemo/qrcode.js
@@ -1,3 +1,25 @@
+/*
+ * 【界面概述】
+ * 绘制一个带有中心logo的二维码
+ *
+ * 【控件介绍】
+ * QRCode：描绘二维码
+ *      value {string} (必须) 二维码代表的字符串
+ *      size {int} (必须) 二维码展示尺寸，二维码为正方形，所以改值代表宽和高，默认值：128
+ *      fgColor {string} 二维码前景色，默认值"#000000"，黑色
+ *      bgColor {string} 二维码背景色，默认值"#ffffff"，白色
+ *      level {string} 二维码的辨析度，可选值{'L':低, 'M':中, 'H':高, 'Q':最精细}，默认值'L'
+ *      imageSettings {Object} 设置中心logo图片，默认值为null，设置格式为：
+ *                          {
+ *                              src: logo的url地址
+ *                              height: logo的宽度
+ *                              height: logo的高度
+ *                          }
+ *
+ * 【技巧说明】
+ * 无
+ */
+
 import React, { Component } from 'react';
 import QRCode from "../jsview-utils/JsViewReactWidget/JsvQrcode"
 import { globalHistory } from '../demoCommon/RouterHistory';
@@ -9,17 +31,13 @@ class App extends FocusBlock {
         this.state = {
             value: 'http://www.baidu.com/',
             size: 128,
-            fgColor: '#000000',
-            bgColor: '#ffffff',
+            fgColor: '#0000FF',
+            bgColor: '#ffff00',
             level: 'L',
-            includeMargin: false,
             includeImage: true,
             imageH: 24,
             imageW: 24,
-            imageX: 0,
-            imageY: 0,
             imageSrc: 'http://oss.image.51vtv.cn/homepage/20191224/0fdcdc8b258fe7baac16b73f58f8989d.jpg',
-            centerImage: true,
         };
     }
 
@@ -40,15 +58,12 @@ class App extends FocusBlock {
                     fgColor={this.state.fgColor}
                     bgColor={this.state.bgColor}
                     level={this.state.level}
-                    includeMargin={this.state.includeMargin}
                     imageSettings={
                         this.state.includeImage
                             ? {
                                 src: this.state.imageSrc,
                                 height: this.state.imageH,
                                 width: this.state.imageW,
-                                x: this.state.centerImage ? null : this.state.imageX,
-                                y: this.state.centerImage ? null : this.state.imageY,
                             }
                             : null
                     }
