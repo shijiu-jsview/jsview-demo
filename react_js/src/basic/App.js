5a37040d1ad54eb8bb9ba8714531d70f7cab8096
ludl <donglin.lu@qcast.cn>
2020-05-13 17:50:25 +0800

    react_js:basic:完成基础实例合集的说明文档

--- react_js/src/basic/App.js
+++ react_js/src/basic/App.js
@@ -1,3 +1,41 @@
+/*
+ * 【界面概述】
+ * div元素style中设置值对应的样例
+ *
+ * 【控件介绍】
+ * 无
+ *
+ * 【技巧说明】
+ * Q: 如何将div布局到指定坐标中？
+ * A: 设置div style中的top和left，坐标值是相对于父div的左上角的坐标值
+ *
+ * Q: 如何在界面上描画图片？
+ * A: 参照div/DivBackground.js，div的style中，
+ *    描绘纯色时，通过backgroundColor来设置，例如
+ *    <div style={{width:200, height:200, backgroundColor: 'rgba(255, 255, 0, 1)'}}>
+ *    描绘图片时，通过backgroundImage来设置，例如
+ *    <div style={{width:200, height:200, backgroundColor:`url(${iconImgPath})`}}>
+ *    注意，不设置width和height的情况是默认尺寸为0px * 0px，不会展示图片
+ *
+ * Q: 如何对图片进行剪切，只展示图片的局部内容？
+ * A: 参照div/DivClip.js，使用style中的overflow:"hidden"属性进行裁剪，或者使用style中clipPath属性进行裁剪
+ *
+ * Q: 如何将展示图片的四个角变成圆角？
+ * A: 参照img/ImgRadius.js，通过设置style中的borderRadius属性完成圆角改变
+ *
+ * Q: 文字如何描绘？
+ * A: 在div标签的内容中可填写需要描绘的文字，文字的字体字号等属性，通过div标签中的style来控制，参照text文件夹中的各个js。
+ *    文字内容中的折行可以使用'\n'来完成
+ *
+ * Q: 如何让div产生动画效果？
+ * A: 动画需要通过以下两种方式实现，具体见anim文件夹各个js:
+ *    方式1：声明keyframe结构，通过div的style中的animation来引用keyFrame，启动动画
+ *    方式2：设置div style中的transition属性，然后调整div的left/top/width/height之后，会按照transition规则进行动画。
+ *    注意点1：keyFrame可以声明在css文件中，也可以通过document.styleSheet来动态加载，可以参考转盘demo(turntableDemo)
+ *    在JsView系统中，css文件中只能声明keyFrame内容，不能声明其他内容
+ *    注意点2：transition目前只支持left/top/width/height属性的跟踪，其他属性目前不支持
+ */
+
 import React from 'react';
 import './App.css';
 import Title from './Title.js';
