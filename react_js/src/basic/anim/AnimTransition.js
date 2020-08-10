7e29501e3014e213b6cdb4a987f923ff1aa7c2f4
luocf <lcf06@163.com>
2020-08-10 11:21:20 +0800

    react_js:AnimTransition描述追加

--- react_js/src/basic/anim/AnimTransition.js
+++ react_js/src/basic/anim/AnimTransition.js
@@ -25,7 +25,7 @@ class AnimTransition extends React.Component {
         transformScale:'scale3d(0.2,0.2,1.0)',
         transformRotate:'rotate3d(0, 0, 1.0, 360deg)',
         transformTranslate:'translate3d(180px,0px,0)',
-        transform: 'translate3d(80px,0px,0) scale3d(2,2,1.0) rotate3d(0.5, 0.5, 1, 360deg)',
+        transform: 'translate3d(80px,0px,0) scale3d(2,2,1.0) rotate3d(0, 0, 1, 360deg)',
         transformOrigin: 'center center'
       })
       //状态变更
@@ -34,8 +34,8 @@ class AnimTransition extends React.Component {
           transformScale:'scale3d(1.2,1.2,1.0)',
           transformRotate:'rotate3d(0, 0, 1.0, -360deg)',
           transformTranslate:'translate3d(-80px,0px,0)',
-          transform: 'translate3d(30px,30px,0) scale3d(0.2,0.2,1.0) rotate3d(0.5, 0.5, 1, -360deg)',
-          transformOrigin: 'left top'
+          transform: 'translate3d(30px,0px,0) scale3d(0.2,0.2,1.0) rotate3d(0, 0, 1, -360deg)',
+          transformOrigin: 'center center'
         })
       }, 3000);
     }, 3000);
@@ -76,24 +76,24 @@ class AnimTransition extends React.Component {
                   top:20,
                   transition:"transform 2.5s linear 0.5s",
                   transform:this.state.transform,
-                  transformOrigin: this.state.transformOrigin}}/>
+                  transformOrigin: this.state.transformOrigin}}>移动缩放旋转</div>
                 <div style={{...blockStyle,
                   top:140,
                   transition:"transform 2.5s linear 0.5s",
                   transformOrigin: 'center center',
-                  transform:this.state.transformTranslate}}/>
+                  transform:this.state.transformTranslate}}>移动</div>
                 <div style={{...blockStyle,
                   left:140,
                   top:20,
                   transition:"transform 2.5s linear 0.5s",
                   transformOrigin: 'center center',
-                  transform:this.state.transformScale}}/>
+                  transform:this.state.transformScale}}>缩放</div>
                 <div style={{...blockStyle,
                   top:75,
                   left:140,
                   transition:"transform 2.5s linear 0.5s",
                   transformOrigin: 'center center',
-                  transform:this.state.transformRotate}}/>
+                  transform:this.state.transformRotate}}>旋转</div>
               </div>
             </div>
     }
