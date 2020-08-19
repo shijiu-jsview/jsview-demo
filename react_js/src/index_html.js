3a01fd92cd8620c8d1acc02555cd1aebe3fe2215
mengxk <xiaokun.meng@qcast.cn>
2020-08-19 16:01:07 +0800

    react_js: 添加npm run-script build-html编译html的release。

---
+++ react_js/src/index_html.js
@@ -0,0 +1,17 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import {loadJsViewProxy,initEntry,confirmEntry} from './jsview-utils/jsview-react/index_hook_html.js'
+
+initEntry();
+
+// 异步根据端的信息，加载JsView或者Html wrapper框架
+loadJsViewProxy(()=>{
+	import("./main.js").then((entry)=>{
+		console.log("main.js loaded...");
+		entry.default(confirmEntry);
+		console.log("main.js done...");
+	});
+}, "/static/js/");
+// 补充说明： /static/js/: (可选配置)填写main.js或者bundle.js相对于index.html的相对位置，用于image/import.then的相对寻址
+
+console.log("index.js loaded");
