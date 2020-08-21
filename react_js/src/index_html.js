e1b1b4a84ccaefbc0482595450ddae8a2d5ed9a1
ludl <donglin.lu@qcast.cn>
2020-08-21 17:41:28 +0800

    react_js: 调整WebView特供版本应用关系

--- react_js/src/index_html.js
+++ react_js/src/index_html.js
@@ -2,15 +2,15 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import {loadJsViewProxy,initEntry,confirmEntry} from './jsview-utils/jsview-react/index_hook_html.js'
 
+import startApp from "./main"
+
 initEntry();
 
 // 异步根据端的信息，加载JsView或者Html wrapper框架
 loadJsViewProxy(()=>{
-	import("./main.js").then((entry)=>{
-		console.log("main.js loaded...");
-		entry.default(confirmEntry);
-		console.log("main.js done...");
-	});
+	console.log("(WebView Version)main.js loaded...");
+	startApp(confirmEntry);
+	console.log("(WebView Version)main.js done...");
 }, "/static/js/");
 // 补充说明： /static/js/: (可选配置)填写main.js或者bundle.js相对于index.html的相对位置，用于image/import.then的相对寻址
 
--- react_js/src/jsview-utils/jsview-react/index_hook_html.js
+++ react_js/src/jsview-utils/jsview-react/index_hook_html.js
@@ -7,21 +7,7 @@ var Forge = window["Forge"]
 
 var sForgeReactAppDefine = null;
 function loadJsViewProxy(callback, js_sub_path) {
-	//if(!!window.JsView) {
-		//initHeaderScriptLoader(js_sub_path);
-		//import("./jsv_hook_wrapper.js").then((app_define)=>{
-			//sForgeReactAppDefine = app_define.ForgeReactApp;
-			//window.JsView.ForgeExt = app_define.ForgeExtension;
-			//window.JsView.React.JsSubPath = js_sub_path;
-			//callback();
-		//});
-	//} else {
-		//import("./forge_html/index.js").then(()=>{
-			//import("./browser_hook_wrapper.js").then(()=>{
-				callback();
-			//});
-		//});
-	//}
+	callback();
 }
 
 // 当confirmEntry和Forge.RunApp都被调用完成后，才会进行ForgeReactApp运行
