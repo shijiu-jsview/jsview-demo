bb708adff5c8391fcc5bb1fecf57c49d7197b41e
ludl <donglin.lu@qcast.cn>
2020-09-18 15:21:26 +0800

    react_js:DemoApp:调整项目的名称,挺高可读性

--- react_js/src/demoHomepage/DemoApp.js
+++ react_js/src/demoHomepage/DemoApp.js
@@ -165,12 +165,12 @@ let demoInfos = [
 		"class": lazy(() => import('../scrollNum/App').then(m => ({ default: m.SubApp }))),
 	},
 	{
-		"name": "TextBox",
+		"name": "多行文字区域内对齐",
 		"path": "/users/TextBox",
 		"class": lazy(() => import('../textBox/App').then(m => ({ default: m.SubApp }))),
     },
     {
-		"name": "文字缩放",
+		"name": "文字清晰度自适应描画区域",
 		"path": "/users/textScale",
 		"class": lazy(() => import('../textScale/App').then(m => ({ default: m.SubApp }))),
 	},
