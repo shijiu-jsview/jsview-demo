9be56026d28f85ed947d8d1c11d1cefd9aa961f9
chench <changhua.chen@qcast.cn>
2020-09-28 11:09:10 +0800

    react_js: JsvPreload: 支持预下载

--- react_js/src/jsview-utils/JsViewReactWidget/JsvPreload.js
+++ react_js/src/jsview-utils/JsViewReactWidget/JsvPreload.js
@@ -1,77 +1,194 @@
 import React from 'react';
-import {Forge, ForgeExtension} from "../jsview-react/index_widget.js"
+import { Forge, ForgeExtension } from "../jsview-react/index_widget.js"
 
-let buildPreloadInfo = (url, width, height, color_type) => {
+let buildPreloadInfo = (url, width, height, color_type, net_setting) => {
     return {
         "url": url,
         "width": width,
         "height": height,
         "colorType": color_type ? color_type : null,
+        "netSetting": net_setting ? net_setting : null,
+    }
+}
+
+let buildDownloadInfo = (url, net_setting) => {
+    return {
+        "url": url,
+        "netSetting": net_setting ? net_setting : null,
     }
 }
 
 class JsvPreload extends React.Component {
     constructor(props) {
         super(props);
-        this._ForgeViewIdList = [];
+        this._PreloadViewList = [];
+        this._DownloadViewList = [];
+
+        this._PreloadStateList = [];
+        this._DownloadStateList = [];
     }
 
     _releaseForgeView() {
-        if (this._ForgeViewIdList.length > 0) {
-            for (let i of this._ForgeViewIdList) {
-                ForgeExtension.RootActivity.ViewStore.remove(i);
+        if (window.JsView) {
+            if (this._PreloadViewList.length > 0) {
+                for (let i of this._PreloadViewList) {
+                    ForgeExtension.RootActivity.ViewStore.remove(i);
+                }
+                this._PreloadViewList = [];
             }
-            this._ForgeViewIdList = [];
+
+            if (this._DownloadViewList.length > 0) {
+                for (let i of this._DownloadViewList) {
+                    ForgeExtension.RootActivity.ViewStore.remove(i);
+                }
+                this._DownloadViewList = [];
+            }
+        } else {
+            this._PreloadViewList = [];
+            this._DownloadViewList = [];
+
+            this._PreloadStateList = [];
+            this._DownloadStateList = [];
         }
     }
 
     shouldComponentUpdate(nextProps, nextState) {
-        if (nextProps.urls.length === this.props.urls.length) {
-            for (let i = 0; i < nextProps.urls.length; i++) {
-                if (nextProps.urls[i].url != this.props.urls[i].url) {
-                    return true;
+        if (nextProps.preloadList.length === this.props.preloadList.length && nextProps.downloadList.length === this.props.downloadList.length) {
+            let same = true;
+            for (let i = 0; i < nextProps.preloadList.length; i++) {
+                if (nextProps.preloadList[i].url != this.props.preloadList[i].url) {
+                    same = false;
+                    break;
                 }
             }
+            for (let i = 0; i < nextProps.downloadList.length; i++) {
+                if (nextProps.downloadList[i].url != this.props.downloadList[i].url) {
+                    same = false;
+                    break;
+                }
+            }
+            return !same
+        } else {
+            return true;
         }
-        return false;
+    }
+
+    _checkPreload() {
+        for (let state of this._PreloadStateList) {
+            if (!state) return;
+        }
+        if (this.props.onPreloadDone) {
+            this.props.onPreloadDone();
+        }
+    }
+
+    _getPreloadViewIdList() {
+        if (!this.props.preloadList) { return }
+        this._PreloadStateList = new Array(this.props.preloadList.length).fill(false);
+        this._PreloadViewList = this.props.preloadList.map((item, index) => {
+            let base_url = item.url;
+            let image_url = base_url;
+            if (base_url && base_url.indexOf("http") < 0) {
+                if (window.JsView.React.UrlRef) {
+                    image_url = new window.JsView.React.UrlRef(base_url).href;
+                }
+            }
+            let texture = ForgeExtension.TextureManager.GetImage(image_url);
+            texture.RegisterLoadImageCallback(null, () => {
+                console.log("preload succeed " + image_url);
+                this._PreloadStateList[index] = true;
+                this._checkPreload()
+            })
+            let texture_setting = new Forge.ExternalTextureSetting(texture);
+            let preload_view = new Forge.PreloadView(texture_setting);
+            return ForgeExtension.RootActivity.ViewStore.add(
+                new Forge.ViewInfo(preload_view, { x: 0, y: 0, width: 0, height: 0 })
+            );
+        });
+    }
+
+    _checkDownload() {
+        for (let state of this._DownloadStateList) {
+            if (!state) return;
+        }
+        if (this.props.onDownloadDone) {
+            this.props.onDownloadDone();
+        }
+    }
+
+    _getDownloadViewIdList() {
+        if (!this.props.downloadList) { return }
+        this._DownloadStateList = new Array(this.props.downloadList.length).fill(false);
+        this._DownloadViewList = this.props.downloadList.map((item, index) => {
+            let base_url = item.url;
+            let image_url = base_url;
+            if (base_url && base_url.indexOf("http") < 0) {
+                if (window.JsView.React.UrlRef) {
+                    image_url = new window.JsView.React.UrlRef(base_url).href;
+                }
+            }
+            let texture = ForgeExtension.TextureManager.GetDownloadTexture(image_url);
+            texture.RegisterLoadImageCallback(null, () => {
+                this._DownloadStateList[index] = true;
+                this._checkDownload();
+            })
+            let texture_setting = new Forge.ExternalTextureSetting(texture);
+            let preload_view = new Forge.PreloadView(texture_setting);
+            return ForgeExtension.RootActivity.ViewStore.add(
+                new Forge.ViewInfo(preload_view, { x: 0, y: 0, width: 0, height: 0 })
+            );
+        });
+    }
+
+    _htmlPreload() {
+        this._PreloadStateList = new Array(this.props.preloadList.length).fill(false);
+        this._PreloadViewList = this.props.preloadList.map((item, index) => {
+            let image = new Image();
+            image.onload = () => {
+                this._PreloadStateList[index] = true;
+                console.log("preload succeed " + item.url);
+                this._checkPreload();
+            }
+            image.src = item.url;
+            return image;
+        });
+
+        this._DownloadStateList = new Array(this.props.downloadList.length).fill(false);
+        this._DownloadViewList = this.props.downloadList.map((item, index) => {
+            let image = new Image();
+            image.onload = () => {
+                this._DownloadStateList[index] = true;
+                console.log("pre download succeed " + item.url);
+                this._checkDownload();
+            }
+            image.src = item.url;
+            return image;
+        });
     }
 
     render() {
         if (window.JsView) {
             this._releaseForgeView();
-            this._ForgeViewIdList = this.props.urls.map(item => {
-                let base_url = item.url;
-                let image_url = base_url;
-                if (base_url && base_url.indexOf("http") < 0) {
-                    if (window.JsView.React.UrlRef) {
-                        image_url = new window.JsView.React.UrlRef(base_url).href;
-                    }
-                }
-                let texture_setting = new Forge.ExternalTextureSetting(ForgeExtension.TextureManager.GetImage(image_url));
-                let preload_view = new Forge.PreloadView(texture_setting);
-                return ForgeExtension.RootActivity.ViewStore.add(
-                    new Forge.ViewInfo(preload_view, {x:0, y:0, width: 0, height: 0})
-                );
-            })
-            return(
+            this._getPreloadViewIdList();
+            this._getDownloadViewIdList();
+
+            return (
                 <React.Fragment>
                     {
-                        this._ForgeViewIdList.map(id => {
-                            return <div key={id} id={id} jsv_innerview={id}/>
+                        this._PreloadViewList.map(id => {
+                            return <div key={id} id={id} jsv_innerview={id} />
+                        })
+                    }
+                    {
+                        this._DownloadViewList.map(id => {
+                            return <div key={id} id={id} jsv_innerview={id} />
                         })
                     }
                 </React.Fragment>
             )
         } else {
-            return (
-                <React.Fragment>
-                    {
-                        this.props.urls.map((item, index) => {
-                            return <div key={index} style={{ width: item.width, height: item.height, backgroundImage: `url(${item.url})`, visibility: "hidden" }} />
-                        })
-                    }
-                </React.Fragment>
-            )
+            this._htmlPreload();
+            return null;
         }
     }
 
@@ -80,7 +197,16 @@ class JsvPreload extends React.Component {
     }
 }
 
-export{
+let emptyFunc = () => { };
+JsvPreload.defaultProps = {
+    preloadList: [],
+    downloadList: [],
+    onPreloadDone: emptyFunc,
+    onDownloadDone: emptyFunc
+}
+
+export {
     buildPreloadInfo,
+    buildDownloadInfo,
     JsvPreload
 }
--- react_js/src/preload/App.js
+++ react_js/src/preload/App.js
@@ -4,9 +4,10 @@
  *
  * 【控件介绍】
  * JsvPreload: 预加载控件
- *          urls(array) 预加载图片信息数组
- * buildPreloadInfo(url, width, height, color_type) 构建图片信息的函数
- *
+ *          preloadList{array} 预加载图片信息数组
+ *          downloadList{array} 预下载的图片信息
+ * buildPreloadInfo(url, width, height, color_type) 构建预加载图片信息的函数
+ * buildDownloadInfo(url, net_setting) 构建预下载图片信息的函数
  * 【技巧说明】
  * Q: 注意事项
  * A: 建议使用buildPreloadInfo构建图片信息
@@ -15,7 +16,7 @@ import React from 'react';
 import { FocusBlock } from "../demoCommon/BlockDefine"
 import createStandaloneApp from "../demoCommon/StandaloneApp"
 import {HORIZONTAL, SimpleWidget, SlideStyle } from "../jsview-utils/jsview-react/index_widget.js"
-import { buildPreloadInfo, JsvPreload } from '../jsview-utils/JsViewReactWidget/JsvPreload'
+import { buildPreloadInfo, buildDownloadInfo, JsvPreload } from '../jsview-utils/JsViewReactWidget/JsvPreload'
 
 import rank from "./rank.png"
 import rankF from "./rank_focus.png"
@@ -23,6 +24,8 @@ import rule from "./rule.png"
 import ruleF from "./rule_focus.png"
 import start from "./start.png"
 import startF from "./start_focus.png"
+import awesome from "./awesomeface.png"
+import cat from "./cat.jpg"
 
 let data = [
     {
@@ -44,6 +47,9 @@ class MainScene extends FocusBlock {
         super(props);
         this._RenderItem = this._RenderItem.bind(this);
         this._RenderFocus = this._RenderFocus.bind(this);
+        this.state = {
+            "text": ""
+        }
     }
 
     _Measures(item) {
@@ -83,6 +89,7 @@ class MainScene extends FocusBlock {
             preload_info.push(buildPreloadInfo(item.url, 166, 90));
             preload_info.push(buildPreloadInfo(item.focusUrl, 166, 90));
         }
+        let download_info = [buildDownloadInfo(awesome), buildDownloadInfo(cat)];
         return (
             <div style={{width: 1920, height: 1080, backgroundColor: "#FFFFFF"}}>
                 <div style={{ top: 50, left: 300 }}>
@@ -97,7 +104,14 @@ class MainScene extends FocusBlock {
                         measures={this._Measures}
                         branchName={this.props.branchName + "/swidget"} />
                 </div>
-                <JsvPreload urls={preload_info} />
+                <div style={{left: 50, top: 200, width: 800, height: 800, fontSize: "30px", color: "#000000"}}>
+                    {this.state.text}
+                </div>
+                <JsvPreload 
+                    preloadList={preload_info}
+                    downloadList={download_info}
+                    onPreloadDone={() => {console.log("PRELOAD DONE!")}}
+                    onDownloadDone={() => {console.log("DOWNLOAD DONE!"); this.setState({text: "图片下载完成\n" + awesome + "\n" + cat})}} />
             </div>
         )
     }
