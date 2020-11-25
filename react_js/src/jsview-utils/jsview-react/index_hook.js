a37c2b3ca48451e7613042149ff6c12d80b9e421
luocf <lcf06@163.com>
2020-11-25 18:05:34 +0800

    react_js:jsview-utils index_hook修正jsview主页面无法启动问题

--- react_js/src/jsview-utils/jsview-react/index_hook.js
+++ react_js/src/jsview-utils/jsview-react/index_hook.js
@@ -33,13 +33,14 @@ function initDesignedMap(input_designed_map) {
   };
 }
 
-// let sForgeReactAppDefine = null;
+// eslint-disable-next-line no-unused-vars
+let sForgeReactAppDefine = null;
 function loadJsViewProxy(callback, js_sub_path, input_designed_map) {
   initDesignedMap(input_designed_map);
   if (window.JsView) {
     initHeaderScriptLoader(js_sub_path);
     import("./jsv_hook_wrapper.js").then((app_define) => {
-      // sForgeReactAppDefine = app_define.ForgeReactApp;
+      sForgeReactAppDefine = app_define.ForgeReactApp;
       window.JsView.ForgeExt = app_define.ForgeExtension;
       window.JsView.React.JsSubPath = js_sub_path;
       callback();
@@ -54,14 +55,18 @@ function loadJsViewProxy(callback, js_sub_path, input_designed_map) {
 }
 
 // 当confirmEntry和Forge.RunApp都被调用完成后，才会进行ForgeReactApp运行
-let sActivityManager = null;
-let sEntryConfirmed = false;
-// let sReactApp = null;
+// eslint-disable-next-line no-var
+var sActivityManager = null;
+// eslint-disable-next-line no-var
+var sEntryConfirmed = false;
+let sReactApp = null;
 
 function startApp() {
   if (sActivityManager !== null && sEntryConfirmed) {
     console.log("Forge.RunApp().");
-    // sReactApp = new sForgeReactAppDefine(sActivityManager);
+    // eslint-disable-next-line new-cap
+    sReactApp = new sForgeReactAppDefine(sActivityManager);
+    console.log(sReactApp);
   }
 }
 
