644b7fd1de198bdd78346d6575167ba24ff487bc
ludl <donglin.lu@qcast.cn>
2020-11-26 16:53:12 +0800

    react_js:index_widget.js:module.exports调整为export * from

--- react_js/src/jsview-utils/jsview-react/index_widget.js
+++ react_js/src/jsview-utils/jsview-react/index_widget.js
@@ -3,4 +3,4 @@ module.exports = require("jsview-react/dist/jsviewwidget.min.js");
 
 // Debug Entry
 // TODO: 进行编译(yarn build)时，需要关闭掉此require
-// module.exports = require("./code/index_widget.js")
+// export * from "./code/index_widget.js"
