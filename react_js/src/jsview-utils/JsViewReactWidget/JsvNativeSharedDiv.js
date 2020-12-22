81557c9dfb2e831004ddd25957052fde7c079875
ludl <donglin.lu@qcast.cn>
2020-12-22 15:41:18 +0800

    react_js:追加testNativeShared的测试样例(内部测试,不做公开项目)

--- react_js/src/jsview-utils/JsViewReactWidget/JsvNativeSharedDiv.js
+++ react_js/src/jsview-utils/JsViewReactWidget/JsvNativeSharedDiv.js
@@ -15,82 +15,101 @@ import PropTypes from "prop-types";
 import { Forge, ForgeExtension } from "../jsview-react/index_widget";
 
 class JsvNativeSharedDiv extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this._CurrentStyle = {
-            left: 0,
-            top: 0,
-            width: 0,
-            height: 0
-        };
+    this._CurrentStyle = {
+      left: 0,
+      top: 0,
+      width: 0,
+      height: 0,
+    };
 
-        this._JsvMainView = null;
-        this._InnerViewId = -1;
+    this._JsvMainView = null;
+    this._InnerViewId = -1;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // 只关心left, top, width, height的变更
+    const new_style = nextProps.style;
+    return (
+      this._CurrentStyle.left !== new_style.left ||
+      this._CurrentStyle.top !== new_style.top ||
+      this._CurrentStyle.width !== new_style.width ||
+      this._CurrentStyle.height !== new_style.height
+    );
+  }
+
+  render() {
+    if (!window.JsView || !this.props.style) {
+      // 非JsView场景，以黑色区域代替，没有可描画的内容
+      return (
+        <div
+          style={{
+            left: this.props.style.left,
+            top: this.props.style.top,
+            width: this.props.style.width,
+            height: this.props.style.height,
+            backgroundColor: "#000000",
+          }}
+          children={this.props.children}
+        />
+      );
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        // 只关心left, top, width, height的变更
-        let new_style = nextProps.style;
-        return (this._CurrentStyle.left !== new_style.left
-            || this._CurrentStyle.top !== new_style.top
-            || this._CurrentStyle.width !== new_style.width
-            || this._CurrentStyle.height !== new_style.height);
+    if (this._JsvMainView === null) {
+      // 初始化View
+
+      // 创建JsView图层穿透的texture，抠洞处理
+      const seeThroughTexture = ForgeExtension.TextureManager.GetColorTexture(
+        "rgba(0,0,0,0)"
+      );
+      const textureSetting = new Forge.TextureSetting(
+        seeThroughTexture,
+        null,
+        null,
+        false
+      );
+
+      // 通过内置函数构造定制的NativeSharedView
+      this._JsvMainView = new Forge.NativeSharedView(textureSetting);
+      this._InnerViewId = ForgeExtension.RootActivity.ViewStore.add(
+        new Forge.ViewInfo(this._JsvMainView, null)
+      );
     }
 
-    render() {
-        if (!window.JsView || !this.props.style) {
-            // 非JsView场景，以黑色区域代替，没有可描画的内容
-            return (<div style={{
-                left: this.props.style.left,
-                top:this.props.style.top,
-                width:this.props.style.width,
-                height:this.props.style.height,
-                backgroundColor: "#000000"
-            }} children={this.props.children} />);
-        }
+    // 更新宽高
+    this._JsvMainView.ResetLayoutParams({
+      width: this.props.style.width,
+      height: this.props.style.height,
+    });
 
-        if (this._JsvMainView === null) {
-            // 初始化View
-
-            // 创建JsView图层穿透的texture，抠洞处理
-            const seeThroughTexture = ForgeExtension.TextureManager.GetColorTexture("rgba(0,0,0,0)");
-            const textureSetting = new Forge.TextureSetting(seeThroughTexture, null, null, false);
-
-            // 通过内置函数构造定制的NativeSharedView
-            this._JsvMainView = new Forge.NativeSharedView(textureSetting);
-            this._InnerViewId = ForgeExtension.RootActivity.ViewStore.add(
-                new Forge.ViewInfo(this._JsvMainView, null)
-            );
-        }
-
-        // 更新宽高
-        this._JsvMainView.ResetLayoutParams({width: this.props.style.width, height: this.props.style.height});
-
-        // 回报Id信息
-        if (this.props.getId) {
-            this.props.getId(this._JsvMainView.GetTrackId());
-        }
-
-        return (
-            <div jsv_innerview={this._InnerViewId} children={this.props.children}
-                     style={{left: this.props.style.left, top:this.props.style.top}}>
-            </div>
-        );
+    // 回报Id信息
+    if (this.props.getId) {
+      this.props.getId(this._JsvMainView.GetTrackId());
     }
 
-    componentWillUnmount() {
-        // 清理View管理缓存
-        if (this._InnerViewId !== -1) {
-            ForgeExtension.RootActivity.ViewStore.remove(this._InnerViewId);
-            this._InnerViewId = -1;
-            this._JsvMainView = null;
-        }
+    return (
+      <div
+        jsv_innerview={this._InnerViewId}
+        children={this.props.children}
+        style={{ left: this.props.style.left, top: this.props.style.top }}
+      ></div>
+    );
+  }
+
+  componentWillUnmount() {
+    // 清理View管理缓存
+    if (this._InnerViewId !== -1) {
+      ForgeExtension.RootActivity.ViewStore.remove(this._InnerViewId);
+      this._InnerViewId = -1;
+      this._JsvMainView = null;
     }
+  }
 }
 JsvNativeSharedDiv.propTypes = {
-    style: PropTypes.object,
-    getId: PropTypes.func
+  style: PropTypes.object,
+  getId: PropTypes.func,
 };
 
 export default JsvNativeSharedDiv;
