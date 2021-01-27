7b6508e6d0fe6f462b0521b91d6aebe0baa17539
ludl <donglin.lu@qcast.cn>
2021-01-27 18:11:57 +0800

    react_js:jsview-utils:追加TargetCoreRevision,将当前jsview-utils对应的引擎版本更新到此文件中

---
+++ react_js/src/jsview-utils/JsViewReactTools/TargetCoreRevision.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ludl on 1/27/21.
+ */
+
+const TargetRevision = {
+    CoreRevision: 623,
+    JseRevision: "1.0.744",
+    JseUrl: "http://cdn.release.qcast.cn/forge_js/master/JsViewES6_react_r744.jsv.2fd2d0e1.js",
+};
+
+export default TargetRevision;
--- react_js/src/main.js
+++ react_js/src/main.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { FdivRoot } from "./jsview-utils/jsview-react/index_widget";
 import "./index.css";
+import TargetRevision from "./jsview-utils/JsViewReactTools/TargetCoreRevision"
 
 import App from "./transitPage/App";
 
@@ -23,15 +24,12 @@ function startApp(confirm_entry) {
 
     // 检查配套引擎的版本
     if (
-      window.JsView.CodeRevision !==
-        623 /* Native引擎版本(由APK启动参数 CORE 决定) */ ||
-      window.Forge.Version !==
-        "1.0.744" /* JS引擎版本(由APK启动参数 ENGINEJS 决定) */
+      window.JsView.CodeRevision !== TargetRevision.CoreRevision /* Native引擎版本(由APK启动参数 CORE 决定) */ ||
+      window.Forge.Version !== TargetRevision.JseRevision /* JS引擎版本(由APK启动参数 ENGINEJS 决定) */
     ) {
       console.warn(
-        "Warning: JsView Engine version miss matched, some effect will be lost"
+          `Warning: JsView Engine version miss matched, some effect will be lost. url should be ${TargetRevision.JseUrl}`
       );
-      /* Engine js 744 版本地址: http://cdn.release.qcast.cn/forge_js/master/JsViewES6_react_r744.jsv.2fd2d0e1.js */
     }
 
     // (可选配置)按键接受的扩展，例如将静音按键(JAVA键值为164)映射为JS键值20001，PS:注意"164"的引号
