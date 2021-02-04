781d52353968f3e7e7dc099fa6d327dc48d24a9a
ludl <donglin.lu@qcast.cn>
2021-02-04 18:22:48 +0800

    java_app/react_js:CoreAAR升级到658版本，解决长文字崩溃和transition内存泄露

--- react_js/src/jsview-utils/JsViewReactTools/TargetCoreRevision.js
+++ react_js/src/jsview-utils/JsViewReactTools/TargetCoreRevision.js
@@ -3,7 +3,7 @@
  */
 
 const TargetRevision = {
-  CoreRevision: 650,
+  CoreRevision: 658,
   JseRevision: "1.0.748",
   JseUrl:
     "http://cdn.release.qcast.cn/forge_js/master/JsViewES6_react_r748.jsv.45651502.js",
