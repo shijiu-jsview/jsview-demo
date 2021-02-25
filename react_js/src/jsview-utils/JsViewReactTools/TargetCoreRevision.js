2a60cdd1b211fa378edac410037d83ac5e4947e9
ludl <donglin.lu@qcast.cn>
2021-02-25 19:06:29 +0800

    java_app/react_js:CoreAAR升级到670, jsview-react升级到200

--- react_js/src/jsview-utils/JsViewReactTools/TargetCoreRevision.js
+++ react_js/src/jsview-utils/JsViewReactTools/TargetCoreRevision.js
@@ -3,7 +3,7 @@
  */
 
 const TargetRevision = {
-  CoreRevision: 664,
+  CoreRevision: 670,
   JseRevision: "1.0.748",
   JseUrl:
     "http://cdn.release.qcast.cn/forge_js/master/JsViewES6_react_r748.jsv.45651502.js",
