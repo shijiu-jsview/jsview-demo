6652262e842a5b162f365836a5351e65d2c368c7
ludl <donglin.lu@qcast.cn>
2021-03-18 10:03:26 +0800

    java_app/react_js:JsView接口AAR升级到690(支持网络通知), Core.AAR升级到690, jsview-react升级到203

--- react_js/src/jsview-utils/JsViewReactTools/TargetCoreRevision.js
+++ react_js/src/jsview-utils/JsViewReactTools/TargetCoreRevision.js
@@ -3,7 +3,7 @@
  */
 
 const TargetRevision = {
-  CoreRevision: 679,
+  CoreRevision: 690,
   JseRevision: "1.0.748",
   JseUrl:
     "http://cdn.release.qcast.cn/forge_js/master/JsViewES6_react_r748.jsv.45651502.js",
