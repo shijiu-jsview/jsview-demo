5e363c528c4d0293c6d0b20a333fe7525c19a4c5
ludl <donglin.lu@qcast.cn>
2021-03-31 14:22:32 +0800

    java_app/react_js:Core.AAR升级到719版本

--- react_js/src/jsview-utils/JsViewReactTools/TargetCoreRevision.js
+++ react_js/src/jsview-utils/JsViewReactTools/TargetCoreRevision.js
@@ -3,7 +3,7 @@
  */
 
 const TargetRevision = {
-  CoreRevision: 697,
+  CoreRevision: 719,
   JseRevision: "1.0.749",
   JseUrl:
     "http://cdn.release.qcast.cn/forge_js/master/JsViewES6_react_r749.jsv.d058fc22.js",
