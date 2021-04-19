f7e5bb4a1d9eb3048577c3b69439c798cddd9796
ludl <donglin.lu@qcast.cn>
2021-04-19 13:34:46 +0800

    java_app/react_js:Core.AAR升级到810741,解决jsidt引用问题

--- react_js/jsview/dom/target_core_revision.js
+++ react_js/jsview/dom/target_core_revision.js
@@ -3,7 +3,7 @@
  */
 
 const TargetRevision = {
-  CoreRevision: 810739,
+  CoreRevision: 810741,
   JseRevision: "1.0.749",
   JseUrl:
     "http://cdn.release.qcast.cn/forge_js/master/JsViewES6_react_r749.jsv.d058fc22.js",
