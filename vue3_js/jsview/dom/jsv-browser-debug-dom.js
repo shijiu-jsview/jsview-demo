2719a2a32ec13dee976396f184df0dce5c9900bf
mengxk <xiaokun.meng@qcast.cn>
2021-04-20 18:30:06 +0800

    vue3_js: 从sample可以运行dom/bin。

--- vue3_js/jsview/dom/jsv-browser-debug-dom.js
+++ vue3_js/jsview/dom/jsv-browser-debug-dom.js
@@ -1,6 +1,6 @@
 // Release entry
-// export * from "jsview-dom/jsview-browser-debug-dom.min"
+export * from "jsview-dom/jsview-browser-debug-dom.min"
 
 // Debug entry
 // TODO: 进行编译(yarn build)时，需要关闭掉此require
-export * from "./code/index_browser_debug_dom"
+// export * from "./code/index_browser_debug_dom"
--- vue3_js/jsview/dom/jsv-forge-define.js
+++ vue3_js/jsview/dom/jsv-forge-define.js
@@ -3,4 +3,4 @@ export * from "jsview-dom/jsview-forge-define.min"
 
 // Debug entry
 // TODO: 进行编译(yarn build)时，需要关闭掉此require
-// export * from "jsview-react/index_jsv_hook"
+// export * from "./code/index_forge_define"
