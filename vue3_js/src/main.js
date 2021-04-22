0528b79f0542ac274ce90bd8c8460762c41a118d
mengxk <xiaokun.meng@qcast.cn>
2021-04-22 17:32:12 +0800

    vue3_js: 修改main.js注释。

--- vue3_js/src/main.js
+++ vue3_js/src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 // 备注：
-// 1. App.vue是一个最简的Vue3工程样例，可在此基础上进行二次开发。
+// 1. App.vue是一个最简的Vue3样例(参考`vue create helloworld`)，可在此基础上进行二次开发。
 // 2. 将第2行[import App from './App.vue']中的
 //    './App.vue'改为'jsview/samples/basic/App', 可以运行samples。
 // 3. 如果你想使用某个sample作为base，可以直接从jsview/samples/xxx拷贝到src目录。
