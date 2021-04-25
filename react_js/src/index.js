e24e8826ba691005e957b32be9cc5ac476facd54
chench <changhua.chen@qcast.cn>
2021-04-25 14:01:56 +0800

    react_js: index.js删除React.StrictMode节点

--- react_js/src/index.js
+++ react_js/src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { FdivRoot } from "../jsview/utils/JsViewEngineWidget/index_widget"; // 焦点控制系统
 
-// import { App } from './App';
-import App from "jsview/samples/transitPage/App"; // 测试主页面
+// import { App } from '../jsview/samples/sprayView/App';
+import App from "../jsview/samples/transitPage/App"; // 测试主页面
 
 ReactDOM.render(
-  <React.StrictMode>
-    <FdivRoot>
-      <App />
-    </FdivRoot>
-  </React.StrictMode>,
+  <FdivRoot>
+    <App />
+  </FdivRoot>,
   document.getElementById("root")
 );
