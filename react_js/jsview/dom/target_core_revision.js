c9e4456c84ae117fcaa46167cc5a6dee19031b2c
ludl <donglin.lu@qcast.cn>
2021-05-07 14:02:11 +0800

    react_js/java_app:Core.AAR升级到810767,解决图片下载异常问题

--- react_js/jsview/dom/target_core_revision.js
+++ react_js/jsview/dom/target_core_revision.js
@@ -3,7 +3,7 @@
  */
 
 const TargetRevision = {
-  CoreRevision: 810765,
+  CoreRevision: 810767,
   JseRevision: "1.0.750",
   JseUrl:
     "http://cdn.release.qcast.cn/forge_js/master/JsViewES6_react_r750.jsv.2444765a.js",
