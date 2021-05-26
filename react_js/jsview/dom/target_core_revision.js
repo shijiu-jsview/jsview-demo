bcc264b3f319635226837de64f7dc1376b2c2787
ludl <donglin.lu@qcast.cn>
2021-05-26 13:08:21 +0800

    java_app/react_js:Core.AAR升级到810785,二维码去边框

--- react_js/jsview/dom/target_core_revision.js
+++ react_js/jsview/dom/target_core_revision.js
@@ -3,7 +3,7 @@
  */
 
 const TargetRevision = {
-  CoreRevision: 810781,
+  CoreRevision: 810785,
   JseRevision: "1.0.756",
   JseUrl:
     "http://cdn.release.qcast.cn/forge_js/master/JsViewES6_react_r756.jsv.98129664.js",
